3650990a31c2b12c87cf011e4d60f2b4b6b1d4dc
jeffersonpordev <seu-email-da-conta@exemplo.com>
2025-04-23 21:53:17 -0300

    segundo commit

--- aula47.py
+++ aula47.py
@@ -14,3 +14,21 @@ na palavra secreta.
 Faça a contagem de tentativas do seu
 usuário.
 """
+palavra_secreta = 'perfume'
+letras_acertadas = ''
+
+while True:
+    letra_digitada = input('Digite uma letra: ')
+
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra.')
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            print(letra_secreta)
+        else:
+            print('*')
