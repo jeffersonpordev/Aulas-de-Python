143e64b3e355ce0f779ca242e4433d8dd2bc7126
jeffersonpordev <seu-email-da-conta@exemplo.com>
2025-05-08 21:39:45 -0300

    commit

---
+++ aula50.py
@@ -0,0 +1,15 @@
+"""
+Exercicio
+Exiba os indices da lista
+0 maria
+1 helena
+2 luiz
+
+"""
+
+
+lista = ['Maria', 'Helena', 'luiz']
+
+for indice, nome in enumerate(lista):
+    print(f'Índice: {indice}, nome: {nome}')
+
