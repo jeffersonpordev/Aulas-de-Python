640e2fdf008b2f46f30608cd7a4cfe6b8dd466e8
jeffersonpordev <seu-email-da-conta@exemplo.com>
2025-05-12 22:09:50 -0300

    commit

---
+++ aula54.py
@@ -0,0 +1,8 @@
+"""
+Faça uma lista de comprar com listas 
+O usuário deve ter a possibilidade de 
+inserir, apagar e listar valores da sua lista
+Não permit que o programa quebre com 
+erros de indíces inexistentes na lista.
+"""
+
