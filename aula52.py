57299b114202b45eca2dfd446d0526cc63ef0262
jeffersonpordev <seu-email-da-conta@exemplo.com>
2025-05-12 21:39:58 -0300

    commit

---
+++ aula52.py
@@ -0,0 +1,18 @@
+"""
+Tipo tupla - Uma lista imutável
+"""
+
+# nomes = 'maria', 'helena', 'luiz'
+# print(nomes[1])
+# print(nomes)
+
+# nomes = ['maria', 'helena', 'luiz']
+# nomes = tuple(nomes)
+# print(nomes[1])
+# print(nomes)
+
+nomes = ['maria', 'helena', 'luiz']
+nomes = tuple(nomes)
+nomes = list(nomes)
+print(nomes[1])
+print(nomes)
