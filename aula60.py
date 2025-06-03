7dd0049b417748e719dd1ea5733acaff1035b09b
jeffersonpordev <seu-email-da-conta@exemplo.com>
2025-06-03 12:55:16 -0300

    commit

---
+++ aula60.py
@@ -0,0 +1,16 @@
+"""
+operação ternaria (condicional de uma linha)
+<valor> if <condição> else <outro valor>
+"""
+# condicao = 10 == 10
+# variavel = 'Valor' if condicao else 'Outro valor' 
+# print(variavel)
+
+# digito = 9 # > 9 = 0
+# novo_digito = digito if digito <= 9 else 0
+
+# novo_digito = 0 if digito > 9 else digito
+# print(novo_digito)
+
+
+print('Valor' if True else 'Outro valor' if True else 'fim')
